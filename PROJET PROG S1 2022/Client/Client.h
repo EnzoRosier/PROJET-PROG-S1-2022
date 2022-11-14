36f4c5ce7030a53956f8565ba07cfbf984d1338c
EnzoRosier <90193056+EnzoRosier@users.noreply.github.com>
2022-11-14 09:58:58 +0100

    ,knzsifz

--- PROJET PROG S1 2022/Client/Client.h
+++ PROJET PROG S1 2022/Client/Client.h
@@ -1,5 +1,5 @@
 #include "../includer.h"
-#include "../Compte/Compte.h"
+
 
 class Client {
 private:
--- PROJET PROG S1 2022/Compte/Compte.cpp
+++ PROJET PROG S1 2022/Compte/Compte.cpp
@@ -1,4 +1,4 @@
-#include "Compte.h"
+#include "../includer.h"
 
 Compte::Compte(const string& id const, int solde = 0) : identifiant_Compte(id), solde_Compte(solde) {
 	cout << "Nouveau compte créé id : " << id << ", solde : " << solde << "\n";
@@ -21,4 +21,8 @@ Compte::get_Solde_Compte() const {
 Compte::set_Solde_Compte(double& newSolde const) {
 	this->solde_Compte = newSolde;
 	cout << "Solde du compte : " << this->identifiant_Compte << "modifié avec la nouvelle valeur : " << this->solde_Compte << "\n"
+}
+
+ostream& Compte::operator<<(ostream& os, Compte c) {
+	return os << c.identifiant_Compte << " -> " << c.solde_Compte << "\n";
 }
