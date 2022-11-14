08c463cb3c4c3c2143dbb6244160ac6be7850094
Gaspard CREPIN <gaspard.crepin@student.junia.com>
2022-11-14 10:00:24 +0100

    Update Banque fin de séance

--- PROJET PROG S1 2022/Banque/Banque.cpp
+++ PROJET PROG S1 2022/Banque/Banque.cpp
@@ -3,11 +3,10 @@
 #include "Banque.h"
 
 Banque_Decentralise::Banque_Decentralise() {
-	int num_banque_temp;
 	Afficher_banque_decentralise();
 	cout << "Entrez le numéro de la banque decentralise : " << endl;
 	cin >> num_banque_temp;
 
-	
-
+	num_banque = num_banque_temp;
+	map <int, Client> registre_local;
 }
