a8f6c5f1d08a2a088e6c534fe6f72ec85c7238e2
Gaspard CREPIN <gaspard.crepin@student.junia.com>
2022-11-14 09:25:51 +0100

    Update Banque

--- PROJET PROG S1 2022/Banque/Banque.h
+++ PROJET PROG S1 2022/Banque/Banque.h
@@ -1,10 +1,11 @@
-#include "includer.h"
+#include "../includer.h"
 
 class Banque_Centrale {
 private: 
 	map <int, Clients> registre;
 public:
 	int Chercher_client(int num_client);
+	int Chercher_num_compte(int num_compte);
 	Clients Chercher_infos_clients(int num_client);
 };
 
@@ -13,5 +14,8 @@ class Banque_Decentralise : Banque_Centrale {
 private:
 	int num_banque;
 	map <int, Clients> registre_local;
+public:
+	void Transaction(int num_compte_receveur);
+	Clients Consulter;
 
 };
