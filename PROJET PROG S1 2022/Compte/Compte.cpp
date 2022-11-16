24f396d6959c0cdf77e76e0da36de7a0e0404194
EnzoRosier <90193056+EnzoRosier@users.noreply.github.com>
2022-11-16 09:20:12 +0100

    Debugged Compte

--- PROJET PROG S1 2022/Compte/Compte.cpp
+++ PROJET PROG S1 2022/Compte/Compte.cpp
@@ -1,28 +1,30 @@
 #include "../includer.h"
+#include "Compte.h"
 
-Compte::Compte(const string& id const, int solde = 0) : identifiant_Compte(id), solde_Compte(solde) {
+Compte::Compte(const string& id, int solde = 0) : identifiant_Compte(id), solde_Compte(solde) {
 	cout << "Nouveau compte créé id : " << id << ", solde : " << solde << "\n";
 }
 
 Compte::~Compte() {
-	cout << "Compte " << this->id << "detruit\n";
+	cout << "Compte " << this->identifiant_Compte << "detruit\n";
 }
 
-Compte::get_Identifiant_Compte() const {
+string Compte::get_Identifiant_Compte() const {
 	cout << "Demande identifiant compte : " << this->identifiant_Compte << "\n";
 	return this->identifiant_Compte;
 }
 
-Compte::get_Solde_Compte() const {
+double Compte::get_Solde_Compte() const {
 	cout << "Demande solde compte : " << this->identifiant_Compte << "\n";
 	return this->solde_Compte;
 }
 
-Compte::set_Solde_Compte(double& newSolde const) {
+int Compte::set_Solde_Compte(double& newSolde) {
 	this->solde_Compte = newSolde;
-	cout << "Solde du compte : " << this->identifiant_Compte << "modifié avec la nouvelle valeur : " << this->solde_Compte << "\n"
+	cout << "Solde du compte : " << this->identifiant_Compte << "modifié avec la nouvelle valeur : " << this->solde_Compte << "\n";
+	return 1;
 }
 
-ostream& Compte::operator<<(ostream& os, Compte c) {
+ostream& operator<<(ostream& os, Compte c) {
 	return os << c.identifiant_Compte << " -> " << c.solde_Compte << "\n";
 }
