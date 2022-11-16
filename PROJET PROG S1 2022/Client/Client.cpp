087dfe1377c4bc7176f9ad1b13895de936bff9cd
Olivier PAPEGHIN <olivier.papeghin@student.junia.com>
2022-11-16 09:39:02 +0100

    Client.h/.cpp

--- PROJET PROG S1 2022/Client/Client.cpp
+++ PROJET PROG S1 2022/Client/Client.cpp
@@ -1,6 +1,6 @@
 #include "../includer.h"
 
-Client::Client(const int id, string nom, string prenom, string adresse, string agence, float taille_en_pouces, vector<Compte*> liste_compte) {
+Client::Client(const int& id, string& nom, string& prenom, string& adresse, string& agence, float& taille_en_pouces, vector<Compte*>& liste_compte) {
 	this->id = id;
 	this->nom = nom;
 	this->prenom = prenom;
@@ -42,34 +42,34 @@ vector<Compte*> Client::Get_liste_comptes() {
 	return this->liste_comptes;
 }
 
-void Client::Set_nom(string nouv_nom) {
+void Client::Set_nom(string& nouv_nom) {
 	nom = nouv_nom;
 }
 
-void Client::Set_prenom(string nouv_prenom) {
+void Client::Set_prenom(string& nouv_prenom) {
 	prenom = nouv_prenom;
 }
 
-void Client::Set_adresse(string nouv_adresse) {
+void Client::Set_adresse(string& nouv_adresse) {
 	adresse = nouv_adresse;
 }
 
-void Client::Set_agence(string nouv_agence) {
+void Client::Set_agence(string& nouv_agence) {
 	agence = nouv_agence;
 }
 
-void Client::Set_taille_en_pouces(float nouv_taille_en_pouces) {
+void Client::Set_taille_en_pouces(float& nouv_taille_en_pouces) {
 	taille_en_pouces = nouv_taille_en_pouces;
 }
 
-void Client::Set_archive_transaction(vector<Transaction> nouv_archive_transaction) {
+void Client::Set_archive_transaction(vector<Transaction>& nouv_archive_transaction) {
 	archive_transaction = nouv_archive_transaction;
 }
 
-void Client::Set_liste_comptes(vector<Compte*> nouv_liste_comptes) {
+void Client::Set_liste_comptes(vector<Compte*>& nouv_liste_comptes) {
 	liste_comptes = nouv_liste_comptes;
 }
 
-void Client::Ajouter_transaction(Transaction transaction) {
-	liste_transaction
+void Client::Ajouter_transaction(Transaction& transaction, string& date) {
+	archive_transaction.emplace(date, Transaction);
 }
