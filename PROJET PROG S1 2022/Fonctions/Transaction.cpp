a6b275307166939704e950b4a42d2ad74c06f0ac
Gaspard CREPIN <gaspard.crepin@student.junia.com>
2022-11-21 09:36:42 +0100

    Fonction Transaction & Update Banque

--- PROJET PROG S1 2022/Fonctions/Transaction.cpp
+++ PROJET PROG S1 2022/Fonctions/Transaction.cpp
@@ -1,6 +1,24 @@
 #include "../includer.h"
 #include "../../Client/Client.h"
+#include "../../Banque/Banque.h"
 
-pair<string, Transaction> transaction(string date, string debiteur, string crediteur, int montant) {
+#ifndef __TRANSACTION
+#define __TRANSACTION
 
+
+pair<string, Transaction> transaction(string date, string debiteur, string crediteur, int montant, Banque_Centrale Banque) {
+	Transaction transac = {montant,debiteur,crediteur};
+
+	Compte Debit = Banque.Chercher_compte_clients(debiteur);
+	Compte Credit = Banque.Chercher_compte_clients(crediteur);
+
+	double solde_debit = Debit.get_Solde_Compte();
+	Debit.set_Solde_Compte(solde_debit - montant);
+
+	double solde_credit = Credit.get_Solde_Compte();
+	Debit.set_Solde_Compte(solde_credit + montant);
+
+	return make_pair(date, transac);
 }
+
+#endif
