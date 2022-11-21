94ecc29809dbea33b589c453337006764eef3dfc
Gaspard CREPIN <gaspard.crepin@student.junia.com>
2022-11-21 08:15:11 +0100

    Update Banque

--- PROJET PROG S1 2022/PROJET PROG S1 2022.cpp
+++ PROJET PROG S1 2022/PROJET PROG S1 2022.cpp
@@ -1,7 +1,6 @@
 ﻿// PROJET PROG S1 2022.cpp : définit le point d'entrée de l'application.
 //
 
-#include "Banque/Banque.h"
 #include "PROJET PROG S1 2022.h"
 
 using namespace std;
@@ -9,8 +8,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello CMake." << endl;
-
-	Banque_Centrale Banque;
 
 
 	return 0;
