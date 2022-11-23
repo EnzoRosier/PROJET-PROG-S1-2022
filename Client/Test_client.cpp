0b718dd999572ffc1d1e2c964b425e78fd68e94e
Olivier PAPEGHIN <olivier.papeghin@student.junia.com>
2022-11-23 08:31:36 +0100

    Test_client

--- Client/Test_client.cpp
+++ Client/Test_client.cpp
@@ -2,9 +2,11 @@
 #include "../Compte/Compte.h"
 
 int main() {
-	Compte c1 = Compte("1", 334);
-	Compte c2 = Compte("2", 323);
-	Compte_Epargnes c3 = Compte_Epargnes("3", 10, 50);
-	Compte* compte = &c;
+	Compte* c1 = new Compte("1", 334);
+	Compte* c2 = new Compte("2", 323);
+	Compte_Epargnes* c3 = new Compte_Epargnes("3", 10, 50);
+	vector<Compte*> liste_comptes = {c1,c2,c3};
+	cout << liste_comptes.size() << endl;
+	
 
 }
