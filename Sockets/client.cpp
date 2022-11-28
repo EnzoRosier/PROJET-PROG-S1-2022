b90b44c497c81a430f411d61e12da3f2191417f0
Olivier PAPEGHIN <olivier.papeghin@student.junia.com>
2022-11-28 08:42:16 +0100

    Socket

--- Sockets/client.cpp
+++ Sockets/client.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-#include <boost/asio.hpp>
+#include "boost/asio.hpp"
 
-using namespace boost::asio;
-using ip::tcp;
 using std::string;
 using std::cout;
 using std::endl;
+using namespace boost::asio;
+using ip::tcp;
+
+
+string read_(tcp::socket& socket) { // La réception du socket met ledit socket dans une chaine de caractère qui pourra être traitée plus tard
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n");
+    string data = boost::asio::buffer_cast<const char*>(buf.data());
+    return data;
+}
+
+
+void send_(tcp::socket& socket, const string& message) { // On crée le socket avec une chaine de caractères
+    const string msg = message + "\n";
+    boost::asio::write(socket, boost::asio::buffer(message));
+}
 
 
 int main() {
