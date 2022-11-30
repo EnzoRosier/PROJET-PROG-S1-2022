46d844274644d5530b948735ff189f59a1212e3e
Olivier PAPEGHIN <olivier.papeghin@student.junia.com>
2022-11-30 17:30:46 +0100

    WXwidget

--- Xwidget/testxw.cpp
+++ Xwidget/testxw.cpp
@@ -1,6 +1,17 @@
 #include "testxw.h"
 
 wxIMPLEMENT_APP(App);
+// On définit les ID suivants :
+// 1:login
+// 2:register
+// 3:ajout compte
+// 4:suppression compte
+// 5:consultation compte
+// 6:retour d'un cran
+// 7:accueil
+// >10: Pour l'affichage des comptes
+
+
 
 bool App::OnInit()
 {
@@ -34,5 +45,57 @@ Fenetre::Fenetre() : wxFrame(nullptr, wxID_ANY, "Test Banque", wxPoint(30, 30), 
 
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
+}
 
+void Fenetre::OnLogin(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnRegister(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnAddAccount(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnDelAccount(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnAccount(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnReturn(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnHome(wxCommandEvent& event) {
+
+}
+
+void Fenetre::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+
+void Fenetre::OnHelp(wxCommandEvent& event) {
+
+}
+
+CreationClient::CreationClient(wxWindow* parent, wxWindowID id, const wxString& title) : wxDialog(parent, id, title) //, position, size, style)
+{
+    auto text_customer_number = new wxStaticText(this, -1, "Customer Number: ", wxPoint(10, 20), wxSize(180, 20));
+    auto client_firstname = new wxStaticText(this, -1, "Firstname : ", wxPoint(10, 50), wxSize(180, 20));
+    auto client_name = new wxStaticText(this, -1, "Name", wxPoint(10, 80), wxSize(180, 20));
+
+
+    edit_firstname_ = new wxTextCtrl(this, -1, "", wxPoint(160, 50), wxSize(100, 20));
+    edit_name_= new wxTextCtrl(this, -1, "", wxPoint(160, 50), wxSize(100, 20));
+
+
+
+    wxButton* ok = new wxButton(this, wxID_OK, _("OK"), wxPoint(10, 120), wxDefaultSize);
+    wxButton* cancel = new wxButton(this, wxID_CANCEL, _("Cancel"), wxPoint(100, 120), wxDefaultSize);
 }
--- Xwidget/testxw.h
+++ Xwidget/testxw.h
@@ -19,4 +19,31 @@ class Fenetre : public wxFrame
 public:
 	Fenetre();
 private:
+	void OnLogin(wxCommandEvent& event);
+	void OnRegister(wxCommandEvent& event);
+	void OnAddAccount(wxCommandEvent& event);
+	void OnDelAccount(wxCommandEvent& event);
+	void OnAccount(wxCommandEvent& event);
+	void OnReturn(wxCommandEvent& event);
+	void OnHome(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+	void OnHelp(wxCommandEvent& event);
+};
+
+
+class CreationClient : public wxDialog
+{
+public:
+	CreationClient(wxWindow* parent, wxWindowID id, const wxString& title);
+
+	wxString get_customer_number();
+	wxString get_name();
+	wxString get_account_numbers();
+
+private:
+
+	void OnOk(wxCommandEvent& event);
+	wxTextCtrl* edit_firstname_;
+	wxTextCtrl* edit_name_;
+	wxTextCtrl* edit_account_numbers_;
 };
