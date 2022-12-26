385dc4522a6f5ba55ec6304e3f8bc8c75dd687a3
DokZokiu <dokzokiu@gmail.com>
2022-12-26 12:07:21 +0100

    ppp

---
+++ PROJET PROG S1 2022/Fonctions/Threadfonctions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../includer.h"
+#include "../../Client/Client.h"
+#include "../../Banque/Banque.h"
+
+#ifndef __THREADFONCTIONS
+#define __THREADFONCTIONS
+
+void epargneThread(Banque_Centrale& bq);
+
+#endif
