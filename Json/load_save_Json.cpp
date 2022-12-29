d196834c4543980694fbf18ee1de4d94f39b80a7
DokZokiu <dokzokiu@gmail.com>
2022-12-29 16:55:24 +0100

    SAVE FO?CTIONEEEEE

--- Json/load_save_Json.cpp
+++ Json/load_save_Json.cpp
@@ -2,7 +2,7 @@
 
 
 int save_json_registre(Banque_Centrale& bq) {
-    std::ofstream file_out("RegistrePrincipale.json");
+    std::ofstream file_out("registre_BC.json");
     write_json(file_out, bq.GeneratePtreeBanque());
     file_out.close();
     return 1;
